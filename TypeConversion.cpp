2c5d8c0a7d551fccc4c8ab464cc4eef9d145d212
MohammadRushaan <mohammadrushaan84@gmail.com>
2026-08-15 22:39:17 +0530

    Type Conversion

---
+++ TypeConversion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    /* Type Conversion: conversion of one dataype to another
+                        Implicit =automatic
+                        explicit  = Precede value with datatyoe(forcefully)
+    */
+
+    double x= (int) 3.14;
+    
+
+    std::cout << x << std::endl;
+    
+    return 0;
+}
